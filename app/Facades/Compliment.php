b025fc9d631120af21830ab6d28193fdc918a7fb
Elisha Ukpong <ishukpong418@gmail.com>
2023-01-02 12:08:14 +0100

    added compliment facade

---
+++ app/Facades/Compliment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Facades;
+
+use \Illuminate\Support\Facades\Facade;
+
+class Compliment extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'compliment';
+    }
+}
